27e7ead263ea0ddae7dcba85f503159bcd6a8438
Abhinayaa <abhinayaa1787@gmail.com>
2019-07-27 22:30:56 -0400

    Updates

---
+++ precache-manifest.3a349ee93ebe2f8903a42b8babb4f646.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0960b4dbdba9a70d7899fd3272347e5",
+    "url": "/Clicky-Game/app/index.html"
+  },
+  {
+    "revision": "742b78e1ad3140b9c738",
+    "url": "/Clicky-Game/app/static/css/main.24bec111.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/Clicky-Game/app/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "742b78e1ad3140b9c738",
+    "url": "/Clicky-Game/app/static/js/main.b18cc7fa.chunk.js"
+  },
+  {
+    "revision": "c7c20900aa0d869d6e60",
+    "url": "/Clicky-Game/app/static/js/runtime~main.2da534f6.js"
+  }
+]);
